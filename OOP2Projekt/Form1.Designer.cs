9a26f635744950e5cae89e459ed6a9c676fbe0ad
LudiF <fabijan.burecic@gmail.com>
2024-08-06 20:30:40 +0200

    Add project files.

---
+++ OOP2Projekt/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace OOP2Projekt
+{
+    partial class LoginForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDobrodosli = new System.Windows.Forms.Label();
+            this.labelKorisnickoIme = new System.Windows.Forms.Label();
+            this.textBoxKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.labelLozinka = new System.Windows.Forms.Label();
+            this.textBoxLozinka = new System.Windows.Forms.TextBox();
+            this.buttonRegistracija = new System.Windows.Forms.Button();
+            this.linkLabelLozinka = new System.Windows.Forms.LinkLabel();
+            this.buttonPrijava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelDobrodosli
+            // 
+            this.labelDobrodosli.AutoSize = true;
+            this.labelDobrodosli.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDobrodosli.Location = new System.Drawing.Point(317, 63);
+            this.labelDobrodosli.Name = "labelDobrodosli";
+            this.labelDobrodosli.Size = new System.Drawing.Size(159, 31);
+            this.labelDobrodosli.TabIndex = 7;
+            this.labelDobrodosli.Text = "Dobrodošli! ";
+            // 
+            // labelKorisnickoIme
+            // 
+            this.labelKorisnickoIme.AutoSize = true;
+            this.labelKorisnickoIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKorisnickoIme.Location = new System.Drawing.Point(320, 149);
+            this.labelKorisnickoIme.Name = "labelKorisnickoIme";
+            this.labelKorisnickoIme.Size = new System.Drawing.Size(97, 16);
+            this.labelKorisnickoIme.TabIndex = 8;
+            this.labelKorisnickoIme.Text = "Korisnicko ime:";
+            // 
+            // textBoxKorisnickoIme
+            // 
+            this.textBoxKorisnickoIme.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxKorisnickoIme.Location = new System.Drawing.Point(323, 180);
+            this.textBoxKorisnickoIme.Name = "textBoxKorisnickoIme";
+            this.textBoxKorisnickoIme.Size = new System.Drawing.Size(188, 20);
+            this.textBoxKorisnickoIme.TabIndex = 9;
+            this.textBoxKorisnickoIme.Text = "Unesite korisnicko ime";
+            // 
+            // labelLozinka
+            // 
+            this.labelLozinka.AutoSize = true;
+            this.labelLozinka.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLozinka.Location = new System.Drawing.Point(320, 220);
+            this.labelLozinka.Name = "labelLozinka";
+            this.labelLozinka.Size = new System.Drawing.Size(56, 16);
+            this.labelLozinka.TabIndex = 10;
+            this.labelLozinka.Text = "Lozinka:";
+            // 
+            // textBoxLozinka
+            // 
+            this.textBoxLozinka.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxLozinka.Location = new System.Drawing.Point(323, 250);
+            this.textBoxLozinka.Name = "textBoxLozinka";
+            this.textBoxLozinka.Size = new System.Drawing.Size(188, 20);
+            this.textBoxLozinka.TabIndex = 11;
+            this.textBoxLozinka.Text = "Unesite lozinku";
+            // 
+            // buttonRegistracija
+            // 
+            this.buttonRegistracija.Location = new System.Drawing.Point(323, 360);
+            this.buttonRegistracija.Name = "buttonRegistracija";
+            this.buttonRegistracija.Size = new System.Drawing.Size(188, 23);
+            this.buttonRegistracija.TabIndex = 14;
+            this.buttonRegistracija.Text = "Registracija";
+            this.buttonRegistracija.UseVisualStyleBackColor = true;
+            this.buttonRegistracija.Click += new System.EventHandler(this.buttonRegistracija_Click);
+            // 
+            // linkLabelLozinka
+            // 
+            this.linkLabelLozinka.AutoSize = true;
+            this.linkLabelLozinka.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelLozinka.Location = new System.Drawing.Point(399, 276);
+            this.linkLabelLozinka.Name = "linkLabelLozinka";
+            this.linkLabelLozinka.Size = new System.Drawing.Size(112, 13);
+            this.linkLabelLozinka.TabIndex = 13;
+            this.linkLabelLozinka.TabStop = true;
+            this.linkLabelLozinka.Text = "Zaboravili ste lozinku?";
+            // 
+            // buttonPrijava
+            // 
+            this.buttonPrijava.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPrijava.Location = new System.Drawing.Point(323, 317);
+            this.buttonPrijava.Name = "buttonPrijava";
+            this.buttonPrijava.Size = new System.Drawing.Size(188, 23);
+            this.buttonPrijava.TabIndex = 12;
+            this.buttonPrijava.Text = "Prijavi se";
+            this.buttonPrijava.UseVisualStyleBackColor = true;
+            this.buttonPrijava.Click += new System.EventHandler(this.buttonPrijava_Click);
+            // 
+            // LoginForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRegistracija);
+            this.Controls.Add(this.linkLabelLozinka);
+            this.Controls.Add(this.buttonPrijava);
+            this.Controls.Add(this.textBoxLozinka);
+            this.Controls.Add(this.labelLozinka);
+            this.Controls.Add(this.textBoxKorisnickoIme);
+            this.Controls.Add(this.labelKorisnickoIme);
+            this.Controls.Add(this.labelDobrodosli);
+            this.Name = "LoginForma";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDobrodosli;
+        private System.Windows.Forms.Label labelKorisnickoIme;
+        private System.Windows.Forms.TextBox textBoxKorisnickoIme;
+        private System.Windows.Forms.Label labelLozinka;
+        private System.Windows.Forms.TextBox textBoxLozinka;
+        private System.Windows.Forms.Button buttonRegistracija;
+        private System.Windows.Forms.LinkLabel linkLabelLozinka;
+        private System.Windows.Forms.Button buttonPrijava;
+    }
+}
+
---
+++ OOP2Projekt/RegistracijskaForma.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace OOP2Projekt
+{
+    partial class RegistracijskaForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonZensko = new System.Windows.Forms.RadioButton();
+            this.radioButtonMusko = new System.Windows.Forms.RadioButton();
+            this.textBoxEmailRegistracija = new System.Windows.Forms.TextBox();
+            this.labelSpol = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.buttonRegistracijaPotvrda = new System.Windows.Forms.Button();
+            this.textBoxRegistracijaPotvrda = new System.Windows.Forms.TextBox();
+            this.textBoxLozinkaRegistracija = new System.Windows.Forms.TextBox();
+            this.labelPotvrdaLozinke = new System.Windows.Forms.Label();
+            this.labelLozinkaReg = new System.Windows.Forms.Label();
+            this.textBoxKorisnickoImeRegistracija = new System.Windows.Forms.TextBox();
+            this.labelRegistracijaKorisnickoIme = new System.Windows.Forms.Label();
+            this.labelRegistracijaPozdrav = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButtonZensko
+            // 
+            this.radioButtonZensko.AutoSize = true;
+            this.radioButtonZensko.Location = new System.Drawing.Point(422, 318);
+            this.radioButtonZensko.Name = "radioButtonZensko";
+            this.radioButtonZensko.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonZensko.TabIndex = 28;
+            this.radioButtonZensko.TabStop = true;
+            this.radioButtonZensko.Text = "Žensko";
+            this.radioButtonZensko.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMusko
+            // 
+            this.radioButtonMusko.AutoSize = true;
+            this.radioButtonMusko.Location = new System.Drawing.Point(339, 318);
+            this.radioButtonMusko.Name = "radioButtonMusko";
+            this.radioButtonMusko.Size = new System.Drawing.Size(57, 17);
+            this.radioButtonMusko.TabIndex = 24;
+            this.radioButtonMusko.TabStop = true;
+            this.radioButtonMusko.Text = "Muško";
+            this.radioButtonMusko.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEmailRegistracija
+            // 
+            this.textBoxEmailRegistracija.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxEmailRegistracija.Location = new System.Drawing.Point(289, 281);
+            this.textBoxEmailRegistracija.Name = "textBoxEmailRegistracija";
+            this.textBoxEmailRegistracija.Size = new System.Drawing.Size(194, 20);
+            this.textBoxEmailRegistracija.TabIndex = 23;
+            // 
+            // labelSpol
+            // 
+            this.labelSpol.AutoSize = true;
+            this.labelSpol.Location = new System.Drawing.Point(286, 320);
+            this.labelSpol.Name = "labelSpol";
+            this.labelSpol.Size = new System.Drawing.Size(31, 13);
+            this.labelSpol.TabIndex = 26;
+            this.labelSpol.Text = "Spol:";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(286, 265);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(34, 13);
+            this.labelEmail.TabIndex = 25;
+            this.labelEmail.Text = "e-mail";
+            this.labelEmail.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonRegistracijaPotvrda
+            // 
+            this.buttonRegistracijaPotvrda.Location = new System.Drawing.Point(289, 358);
+            this.buttonRegistracijaPotvrda.Name = "buttonRegistracijaPotvrda";
+            this.buttonRegistracijaPotvrda.Size = new System.Drawing.Size(194, 23);
+            this.buttonRegistracijaPotvrda.TabIndex = 27;
+            this.buttonRegistracijaPotvrda.Text = "Registriraj se";
+            this.buttonRegistracijaPotvrda.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRegistracijaPotvrda
+            // 
+            this.textBoxRegistracijaPotvrda.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxRegistracijaPotvrda.Location = new System.Drawing.Point(289, 232);
+            this.textBoxRegistracijaPotvrda.Name = "textBoxRegistracijaPotvrda";
+            this.textBoxRegistracijaPotvrda.Size = new System.Drawing.Size(194, 20);
+            this.textBoxRegistracijaPotvrda.TabIndex = 22;
+            // 
+            // textBoxLozinkaRegistracija
+            // 
+            this.textBoxLozinkaRegistracija.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxLozinkaRegistracija.Location = new System.Drawing.Point(289, 181);
+            this.textBoxLozinkaRegistracija.Name = "textBoxLozinkaRegistracija";
+            this.textBoxLozinkaRegistracija.Size = new System.Drawing.Size(194, 20);
+            this.textBoxLozinkaRegistracija.TabIndex = 21;
+            // 
+            // labelPotvrdaLozinke
+            // 
+            this.labelPotvrdaLozinke.AutoSize = true;
+            this.labelPotvrdaLozinke.Location = new System.Drawing.Point(286, 216);
+            this.labelPotvrdaLozinke.Name = "labelPotvrdaLozinke";
+            this.labelPotvrdaLozinke.Size = new System.Drawing.Size(76, 13);
+            this.labelPotvrdaLozinke.TabIndex = 20;
+            this.labelPotvrdaLozinke.Text = "Potvrdi lozinku";
+            // 
+            // labelLozinkaReg
+            // 
+            this.labelLozinkaReg.AutoSize = true;
+            this.labelLozinkaReg.Location = new System.Drawing.Point(286, 165);
+            this.labelLozinkaReg.Name = "labelLozinkaReg";
+            this.labelLozinkaReg.Size = new System.Drawing.Size(44, 13);
+            this.labelLozinkaReg.TabIndex = 19;
+            this.labelLozinkaReg.Text = "Lozinka";
+            // 
+            // textBoxKorisnickoImeRegistracija
+            // 
+            this.textBoxKorisnickoImeRegistracija.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxKorisnickoImeRegistracija.Location = new System.Drawing.Point(289, 132);
+            this.textBoxKorisnickoImeRegistracija.Name = "textBoxKorisnickoImeRegistracija";
+            this.textBoxKorisnickoImeRegistracija.Size = new System.Drawing.Size(194, 20);
+            this.textBoxKorisnickoImeRegistracija.TabIndex = 18;
+            // 
+            // labelRegistracijaKorisnickoIme
+            // 
+            this.labelRegistracijaKorisnickoIme.AutoSize = true;
+            this.labelRegistracijaKorisnickoIme.Location = new System.Drawing.Point(286, 116);
+            this.labelRegistracijaKorisnickoIme.Name = "labelRegistracijaKorisnickoIme";
+            this.labelRegistracijaKorisnickoIme.Size = new System.Drawing.Size(75, 13);
+            this.labelRegistracijaKorisnickoIme.TabIndex = 17;
+            this.labelRegistracijaKorisnickoIme.Text = "Korisničko ime";
+            // 
+            // labelRegistracijaPozdrav
+            // 
+            this.labelRegistracijaPozdrav.AutoSize = true;
+            this.labelRegistracijaPozdrav.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRegistracijaPozdrav.Location = new System.Drawing.Point(190, 69);
+            this.labelRegistracijaPozdrav.Name = "labelRegistracijaPozdrav";
+            this.labelRegistracijaPozdrav.Size = new System.Drawing.Size(421, 25);
+            this.labelRegistracijaPozdrav.TabIndex = 16;
+            this.labelRegistracijaPozdrav.Text = "Registriraj se i započni pratiti svoje vježbe!";
+            // 
+            // RegistracijskaForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonZensko);
+            this.Controls.Add(this.radioButtonMusko);
+            this.Controls.Add(this.textBoxEmailRegistracija);
+            this.Controls.Add(this.labelSpol);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.buttonRegistracijaPotvrda);
+            this.Controls.Add(this.textBoxRegistracijaPotvrda);
+            this.Controls.Add(this.textBoxLozinkaRegistracija);
+            this.Controls.Add(this.labelPotvrdaLozinke);
+            this.Controls.Add(this.labelLozinkaReg);
+            this.Controls.Add(this.textBoxKorisnickoImeRegistracija);
+            this.Controls.Add(this.labelRegistracijaKorisnickoIme);
+            this.Controls.Add(this.labelRegistracijaPozdrav);
+            this.Name = "RegistracijskaForma";
+            this.Text = "Registracija";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonZensko;
+        private System.Windows.Forms.RadioButton radioButtonMusko;
+        private System.Windows.Forms.TextBox textBoxEmailRegistracija;
+        private System.Windows.Forms.Label labelSpol;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Button buttonRegistracijaPotvrda;
+        private System.Windows.Forms.TextBox textBoxRegistracijaPotvrda;
+        private System.Windows.Forms.TextBox textBoxLozinkaRegistracija;
+        private System.Windows.Forms.Label labelPotvrdaLozinke;
+        private System.Windows.Forms.Label labelLozinkaReg;
+        private System.Windows.Forms.TextBox textBoxKorisnickoImeRegistracija;
+        private System.Windows.Forms.Label labelRegistracijaKorisnickoIme;
+        private System.Windows.Forms.Label labelRegistracijaPozdrav;
+    }
+}
